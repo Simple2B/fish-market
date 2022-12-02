1050a5bc42fb43e0476129e55d4e2a8f756ec6cb
nazarKobryn <kobrinnazar05@gmail.com>
2022-12-02 17:12:50 +0200

    maker step switcher and create new component Logo

--- frontend/src/pages/Market/Market.tsx
+++ frontend/src/pages/Market/Market.tsx
@@ -1,12 +1,21 @@
-import { useQuery } from "@tanstack/react-query";
 import { useState, useReducer } from "react";
 import { Navigate, useParams } from "react-router-dom";
 
 import { ProductList } from "./components/ProductList";
 import { initialState, reducer } from "./Market.reducer";
-import { IBusinessOut, MarketActionTypes } from "./Market.type";
 
 import style from "./Market.module.css";
+import { Logo } from "./components/Logo";
+
+enum BusinessStep {
+  START_ORDER,
+  ORDER,
+}
+
+const buttonTitle = {
+  [BusinessStep.ORDER]: "Order",
+  [BusinessStep.START_ORDER]: "Start Order",
+};
 
 export function Market() {
   const { marketId } = useParams<"marketId">();
@@ -16,69 +25,26 @@ export function Market() {
 
   const [cartState, dispatchCart] = useReducer(reducer, initialState);
 
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ["marketDetails"],
-    queryFn: async () => {
-      const res = await fetch(
-        `${import.meta.env.VITE_API_BASE_URL}/business/${marketId}`
-      );
+  const [step, setStep] = useState<BusinessStep>(BusinessStep.START_ORDER);
 
-      if (!res.ok) {
-        throw new Error("Business not found");
-      }
-
-      const data: IBusinessOut = await res.json();
-      return data;
-    },
-  });
-
-  const [showProducts, setShowProducts] = useState<boolean>(false);
-  const handleStartOrder = () => {
-    setShowProducts(true);
+  const handleStepBusiness = () => {
+    if (step === BusinessStep.ORDER && cartState.length < 1) return;
+    setStep((value) => value + 1);
   };
 
-  const logoElement = data?.logo ? (
-    <img
-      style={{ width: "100%", height: "100%" }}
-      src={data.logo}
-      alt="Business logo"
-    />
-  ) : (
-    "logo"
-  );
-
-  if (isError) {
-    return <Navigate to={"/"} replace={true} />;
-  }
-
-  return isLoading ? (
-    <p>LOADING...</p>
-  ) : (
+  return (
     <>
-      {!showProducts && (
-        <div className={style.marketPageStart}>
-          <div className={style.businessLogo}>
-            <div className={style.businessLogoWrap}>{logoElement}</div>
-          </div>
-          <div className={style.businessTitle}>
-            <div className={style.businessTitleText}>
-              Welcome to {data!.name}
-            </div>
-          </div>
-          <div className={style.businessBtnStart} onClick={handleStartOrder}>
-            <div className={style.businessBtnStartText}>Start Order</div>
-          </div>
-        </div>
+      {step === BusinessStep.START_ORDER && <Logo marketId={marketId} />}
+      {step === BusinessStep.ORDER && (
+        <ProductList
+          marketId={marketId}
+          cartState={cartState}
+          dispatchCart={dispatchCart}
+        />
       )}
-      {showProducts && (
-        <>
-          <ProductList
-            marketId={marketId}
-            cartState={cartState}
-            dispatchCart={dispatchCart}
-          />
-        </>
-      )}
+      <div className={style.businessBtn} onClick={handleStepBusiness}>
+        {buttonTitle[step]}
+      </div>
     </>
   );
 }
---
+++ frontend/src/pages/Market/components/Logo/Logo.tsx
@@ -0,0 +1,56 @@
+import { useQuery } from "@tanstack/react-query";
+import { Navigate } from "react-router-dom";
+import { IBusinessOut } from "../../Market.type";
+
+import style from "./Logo.module.css";
+
+interface LogoProps {
+  marketId: string;
+}
+
+const Logo = ({ marketId }: LogoProps) => {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["marketDetails"],
+    queryFn: async () => {
+      const res = await fetch(
+        `${import.meta.env.VITE_API_BASE_URL}/business/${marketId}`
+      );
+
+      if (!res.ok) {
+        throw new Error("Business not found");
+      }
+
+      const data: IBusinessOut = await res.json();
+      return data;
+    },
+  });
+
+  const logoElement = data?.logo ? (
+    <img
+      style={{ width: "100%", height: "100%" }}
+      src={data.logo}
+      alt="Business logo"
+    />
+  ) : (
+    "logo"
+  );
+
+  if (isError) {
+    return <Navigate to={"/"} replace={true} />;
+  }
+
+  return isLoading ? (
+    <p>LOADING...</p>
+  ) : (
+    <div className={style.marketLogoStart}>
+      <div className={style.businessLogo}>
+        <div className={style.businessLogoWrap}>{logoElement}</div>
+      </div>
+      <div className={style.businessTitle}>
+        <div className={style.businessTitleText}>Welcome to {data!.name}</div>
+      </div>
+    </div>
+  );
+};
+
+export { Logo };
