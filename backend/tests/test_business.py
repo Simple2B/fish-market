deeb4eeb71d7043fa0ec19de3c22ebc7f8cf326f
nazarKobryn <kobrinnazar05@gmail.com>
2022-12-05 17:07:05 +0200

    fixed test_get_user_marketeer_business

--- backend/tests/test_business.py
+++ backend/tests/test_business.py
@@ -16,10 +16,11 @@ def test_get_user_marketeer_business(marketer_client: TestClient, db: Session):
     user: m.User = db.query(m.User).filter_by(role=m.UserRole.Marketeer).first()
     user_business: m.Business = user.businesses[0]
 
-    res = marketer_client.get("/business/")
+    res = marketer_client.get(f"/business/{user_business.web_site_id}")
     assert res.status_code == status.HTTP_200_OK
     res_data = s.BusinessOut.parse_obj(res.json())
-    assert user_business.web_site_id == res_data.web_site_id
+    assert user_business.logo == res_data.logo
+    assert user_business.name == res_data.name
     assert user_business.user_id == user.id
 
 
