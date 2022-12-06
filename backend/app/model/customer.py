dd9e859c42e733a6d995864f618bb59053d9d330
nazarKobryn <kobrinnazar05@gmail.com>
2022-12-06 10:34:09 +0200

    fixed flake8

--- backend/app/model/customer.py
+++ backend/app/model/customer.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer, Boolean, ForeignKey
+from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 
 from app.database import Base
