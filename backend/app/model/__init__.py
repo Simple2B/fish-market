52d0deda1c9aefcd9ece29fbac3a6e965539937d
nazarKobryn <kobrinnazar05@gmail.com>
2022-12-06 17:19:16 +0200

    delete model PhoneNumber

--- backend/app/model/__init__.py
+++ backend/app/model/__init__.py
@@ -11,5 +11,4 @@ from .customer import Customer
 from .order import Order
 from .order_item import OrderItem
 from .order_history import OrderHistory
-from .phone_number import PhoneNumber
 from .utils import gen_unique_uid, gen_confirm_code
