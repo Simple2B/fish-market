812e893a37e26d11813fa68b43398adde89d5c83
nazarKobryn <kobrinnazar05@gmail.com>
2022-12-11 18:54:46 +0200

    finished style page ConfirmCodeForm

--- frontend/src/pages/Market/components/Confirm/ConfirmCodeForm.tsx
+++ frontend/src/pages/Market/components/Confirm/ConfirmCodeForm.tsx
@@ -1,10 +1,53 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { ErrorMessage } from "../PersonalInfo/ErrorMessage";
 import style from "./ConfirmCodeForm.module.css";
 
 type ConfirmCodeFormProps = {};
 
 const ConfirmCodeForm = (props: ConfirmCodeFormProps) => {
-  return <div className={style.confirmCodeFormPage}>ConfirmCodeForm</div>;
+  const {
+    register,
+    handleSubmit,
+    setError,
+    getValues,
+    formState: { errors },
+  } = useForm();
+
+  return (
+    <div className={style.confirmCodeFormPage}>
+      <form
+        className={style.formContent}
+        onSubmit={handleSubmit(() => console.log("hi"))}
+      >
+        <div className={style.formLabelText}>Confirmation code</div>
+        <input
+          {...register("sms_code", {
+            required: true,
+            minLength: 6,
+            maxLength: 6,
+          })}
+          className={style.smsCode}
+          placeholder="Enter your code"
+        />
+        {errors.sms_code && (
+          <ErrorMessage text="The verification code is incorrect" />
+        )}
+        <button type="submit" className={style.submitBtn}>
+          Confirm
+        </button>
+      </form>
+      <div className={style.contentSendAgain}>
+        <div>Did not receive a code?</div>
+        <div
+          className={style.btnSendAgain}
+          onClick={() => console.log("Send again.")}
+        >
+          Send again.
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export { ConfirmCodeForm };
