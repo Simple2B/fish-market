e93efc8e9aeef99ccf826cc54a8b8fb0d1af5f1e
nazarKobryn <kobrinnazar05@gmail.com>
2022-12-15 17:47:54 +0200

    added new constants

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,1 +1,3 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const TOKEN_KEY = "TOKEN";
+export const CHECK_TOKEN = "CHECK_TOKEN";
