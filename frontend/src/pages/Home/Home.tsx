608484b0cc29e47be674fe82aebc6bdc0f158dcd
nazarKobryn <kobrinnazar05@gmail.com>
2022-12-15 18:19:04 +0200

    update Home

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,10 +1,28 @@
-import { MarketNotFound } from "../Market/components/MarketNotFound/MarketNotFound";
+import { useQuery } from "@tanstack/react-query";
+import { Spinner } from "../../components";
+import { API_BASE_URL, CHECK_TOKEN, TOKEN_KEY } from "../../constants";
 import { LoginUser } from "./components/LoginUser";
 
 export function Home() {
-  return (
-    <>
-      <LoginUser />
-    </>
-  );
+  const { data, isLoading } = useQuery({
+    queryKey: [CHECK_TOKEN],
+    queryFn: async () => {
+      const res = await fetch(`${API_BASE_URL}/is-authorization`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem(TOKEN_KEY)}`,
+        },
+      });
+
+      if (!res.ok) {
+        // localStorage.removeItem(TOKEN_KEY);
+        return false;
+      }
+
+      const data = await res.json();
+      return data.is_valid;
+    },
+  });
+
+  return isLoading ? <Spinner /> : <>{data ? <h1>hello</h1> : <LoginUser />}</>;
 }
