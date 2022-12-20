67a242b77747b3a4b4050b88147d47eae7435037
nazarKobryn <kobrinnazar05@gmail.com>
2022-12-20 19:13:15 +0200

    updated Order

--- frontend/src/pages/Home/components/Orders/Orders.tsx
+++ frontend/src/pages/Home/components/Orders/Orders.tsx
@@ -1,19 +1,40 @@
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+
 import { Spinner } from "../../../../components";
 import { API_BASE_URL } from "../../../../constants";
-import { TOKEN_KEY } from "../../../../services";
+import {
+  FilteringFunctions,
+  GET_ORDERS,
+  setFilterCreated,
+  setFilterInProgress,
+  setFilterPending,
+  TOKEN_KEY,
+} from "../../../../services";
+import { OrderData } from "./Order.type";
 import { OrderItem } from "./OrderItem";
 import style from "./Orders.module.css";
+import { FilterButton } from "./FilterButton";
 
 type OrdersProps = {};
 
-// interface IOrderItem {
-//   id: number;
-// }
+enum FilterBtnName {
+  FUTURE_ORDERS = "Future orders",
+  PENDING = "Pending",
+  IN_PROGRESS = "In progress",
+}
 
 const Orders = (props: OrdersProps) => {
+  const FILTER_OPTIONS = [
+    { name: FilterBtnName.FUTURE_ORDERS, filterFn: setFilterCreated },
+    { name: FilterBtnName.PENDING, filterFn: setFilterPending },
+    { name: FilterBtnName.IN_PROGRESS, filterFn: setFilterInProgress },
+  ];
+  const [ordersData, setOrdersData] = useState<OrderData[]>([]);
+  const [activeBtn, setActiveBtn] = useState<string>(FilterBtnName.PENDING);
+
   const { data, isLoading } = useQuery({
-    queryKey: ["getOrders"],
+    queryKey: [GET_ORDERS],
     queryFn: async () => {
       const res = await fetch(`${API_BASE_URL}/order/`, {
         method: "GET",
@@ -32,19 +53,46 @@ const Orders = (props: OrdersProps) => {
 
       return data.orders;
     },
+    onSuccess: (data) => {
+      const filterFn = FILTER_OPTIONS.find(
+        (option) => option.name === activeBtn
+      )?.filterFn;
+      if (filterFn) {
+        setOrdersData(data.filter(filterFn));
+      }
+    },
   });
+
+  const handlerButtonsFilters = ({
+    filterFn,
+    name,
+  }: {
+    filterFn: FilteringFunctions;
+    name: string;
+  }) => {
+    if (data) {
+      setOrdersData([...data].filter(filterFn));
+      setActiveBtn(name);
+    }
+  };
 
   return isLoading ? (
     <Spinner />
   ) : (
     <div className={style.ordersPage}>
       <div className={style.buttonsFilter}>
-        <div className={style.buttonFilter}>Future orders</div>
-        <div className={style.buttonFilter}>Pending</div>
-        <div className={style.buttonFilter}>In progress</div>
+        {FILTER_OPTIONS.map((item) => (
+          <FilterButton
+            key={item.name}
+            item={item}
+            handlerButtonsFilters={handlerButtonsFilters}
+            activeBtn={activeBtn}
+          />
+        ))}
       </div>
       <div className={style.ordersContent}>
-        {data && data.map((el: any) => <OrderItem key={el.id} {...el} />)}
+        {ordersData &&
+          ordersData.map((el: OrderData) => <OrderItem key={el.id} {...el} />)}
       </div>
     </div>
   );
