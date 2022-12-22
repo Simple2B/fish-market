8102ed718a9c08a8dc07473b3c9b1d3a904b7735
nazarKobryn <kobrinnazar05@gmail.com>
2022-12-22 15:09:35 +0200

    created main constant TEXT DATA

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,1 +1,20 @@
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const ENGLISH = "En";
+
+export const modalData = {
+  CAN_NOT_COMPLETED: "canNotCompletedKey",
+  REMOVE_ORDER: "removeOrderKey",
+};
+
+export const TEXT_DATA = {
+  ENGLISH: {
+    [modalData.CAN_NOT_COMPLETED]: {
+      title: "Are you sure you can’t complete *order number*?",
+      btnName: "Can’t complete",
+    },
+    [modalData.REMOVE_ORDER]: {
+      title: "Are you sure you want to remove *order number*?",
+      btnName: "Remove order",
+    },
+  },
+};
