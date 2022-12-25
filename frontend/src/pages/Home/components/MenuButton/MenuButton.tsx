8cffd191c5a1cad195c6a1090f2f408aff68b6ac
nazarKobryn <kobrinnazar05@gmail.com>
2022-12-25 14:55:38 +0200

    updated component MenuButton

--- frontend/src/pages/Home/components/MenuButton/MenuButton.tsx
+++ frontend/src/pages/Home/components/MenuButton/MenuButton.tsx
@@ -1,21 +1,44 @@
 import { NavLink } from "react-router-dom";
-import { rebuildUrl } from "../../../../services";
+import { GET_ORDERS, rebuildUrl } from "../../../../services";
 import classNames from "classnames";
 import style from "./MenuButton.module.css";
+import { queryClient } from "../../../../queryClient";
+import {
+  filterBtnNameKeys,
+  navMenuBtnNameKeys,
+  TEXT_DATA,
+} from "../../../../constants";
 
 type MenuButtonProps = {
   btnName: string;
+  setActiveBtn: (n: string) => void;
 };
 
-const MenuButton = ({ btnName }: MenuButtonProps) => {
-  const getNavLinkStyle = ({ isActive }: { isActive: boolean }) =>
+const MenuButton = ({ btnName, setActiveBtn }: MenuButtonProps) => {
+  const handlerMenuButton = () => {
+    if (btnName == TEXT_DATA[navMenuBtnNameKeys.ORDERS].name) {
+      setActiveBtn(TEXT_DATA[filterBtnNameKeys.PENDING].name!);
+      queryClient.invalidateQueries([GET_ORDERS]);
+    }
+
+    if (btnName == TEXT_DATA[navMenuBtnNameKeys.ARCHIVE].name) {
+      setActiveBtn(TEXT_DATA[filterBtnNameKeys.COMPLETED].name!);
+      queryClient.invalidateQueries([GET_ORDERS]);
+    }
+  };
+
+  const getNavLinkStyle = (state: any) =>
     classNames([
       style.menuButtonContent,
-      { [style.menuButtonContentActive]: isActive },
+      { [style.menuButtonContentActive]: state.isActive },
     ]);
 
   return (
-    <NavLink className={getNavLinkStyle} to={`${rebuildUrl(btnName)}`}>
+    <NavLink
+      className={getNavLinkStyle}
+      to={`${rebuildUrl(btnName)}`}
+      onClick={handlerMenuButton}
+    >
       {btnName}
     </NavLink>
   );
