c5ef872c305c06e2fb8dacccd40ffe1016016001
nazarKobryn <kobrinnazar05@gmail.com>
2022-12-25 14:53:38 +0200

    created new TEXT_DATA keys and use in router

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -5,6 +5,21 @@ export const modalData = {
   REMOVE_ORDER: "removeOrderKey",
 };
 
+export const filterBtnNameKeys = {
+  FUTURE_ORDERS: "futureOrdersKey",
+  PENDING: "pendingKey",
+  IN_PROGRESS: "inProgressKey",
+  COMPLETED: "completedKey",
+  CANCELLED: "cancelledKey",
+};
+
+export const navMenuBtnNameKeys = {
+  ORDERS: "ordersKey",
+  ARCHIVE: "archiveKey",
+  OUT_OF_STOCK: "outOfStokeKey",
+  SETTINGS: "settingsKey",
+};
+
 export const TEXT_DATA = {
   [modalData.CAN_NOT_COMPLETED]: {
     title: "Are you sure you can’t complete *order number*?",
@@ -16,4 +31,15 @@ export const TEXT_DATA = {
     btnName: "Remove order",
     toastMessage: "*order number* was successfully removed",
   },
+  [filterBtnNameKeys.PENDING]: {
+    name: "Pending",
+  },
+  [filterBtnNameKeys.IN_PROGRESS]: { name: "In progress" },
+  [filterBtnNameKeys.FUTURE_ORDERS]: { name: "Future orders" },
+  [filterBtnNameKeys.COMPLETED]: { name: "Completed" },
+  [filterBtnNameKeys.CANCELLED]: { name: "Cancelled" },
+  [navMenuBtnNameKeys.ORDERS]: { name: "Orders" },
+  [navMenuBtnNameKeys.ARCHIVE]: { name: "Archive" },
+  [navMenuBtnNameKeys.OUT_OF_STOCK]: { name: "Out of Stock" },
+  [navMenuBtnNameKeys.SETTINGS]: { name: "Settings" },
 };
