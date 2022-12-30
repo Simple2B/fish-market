e278a5f2214fbdfde9e813fd257765e20b686a6c
nazarKobryn <kobrinnazar05@gmail.com>
2022-12-30 16:19:03 +0200

    created component BusinessInfo

---
+++ frontend/src/pages/Home/components/SettingsView/components/LeftPanel/BusinessInfo.tsx
@@ -0,0 +1,35 @@
+import style from "./BusinessInfo.module.css";
+import { LeftPanelType } from "../../../../../../main.type";
+import { AiTwotoneEdit } from "react-icons/ai";
+
+const BusinessInfo = ({
+  logo,
+  name,
+  user_email,
+  handlerEditBtn,
+}: LeftPanelType & { handlerEditBtn: () => void }) => {
+  console.log(user_email);
+
+  return (
+    <div className={style.businessInfoContent}>
+      <div className={style.imgContent}>
+        <img className={style.imgContentWrap} src={logo} alt="logo" />
+      </div>
+      <div className={style.businessInfoContentWrap}>
+        <div>
+          <span>Business name: </span>
+          {name}
+        </div>
+        <div>
+          <span> Email: </span>
+          {user_email}
+        </div>
+      </div>
+      <div className={style.btnEdit} onClick={handlerEditBtn}>
+        <AiTwotoneEdit className={style.btnEditContent} />
+      </div>
+    </div>
+  );
+};
+
+export { BusinessInfo };
