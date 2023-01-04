47a1ac3abfed637bb6a42c5a9afca6235f349960
nazarKobryn <kobrinnazar05@gmail.com>
2023-01-04 11:58:19 +0200

    created ShowUpdatedProduct

---
+++ frontend/src/pages/Home/components/SettingsView/components/ProductsView/ShowUpdateProduct.tsx
@@ -0,0 +1,51 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { SetProductSoldBy } from "../../../../../../components";
+import { ItemUnit } from "../../../../../../main.type";
+import { queryClient } from "../../../../../../queryClient";
+import {
+  getBusinessProductById,
+  GET_BUSINESS_PRODUCTS_BY_ID,
+  updateBusinessProductById,
+} from "../../../../../../services";
+import { ProductInfo } from "./ProductInfo";
+import style from "./ShowUpdateProduct.module.css";
+
+export type ProductId = {
+  id: number;
+};
+
+const ShowUpdateProduct = ({ id }: ProductId) => {
+  const { data } = useQuery({
+    queryKey: [GET_BUSINESS_PRODUCTS_BY_ID, id],
+    queryFn: () => getBusinessProductById(id),
+  });
+
+  const mutationUpdateProductValues = useMutation({
+    mutationFn: updateBusinessProductById,
+    onSuccess: () => {
+      queryClient.invalidateQueries([GET_BUSINESS_PRODUCTS_BY_ID, id]);
+      console.log("success");
+    },
+  });
+
+  const handlerSoldByProduct = (status: ItemUnit) => {
+    const reqData = { product_id: id, body: { sold_by: status } };
+    mutationUpdateProductValues.mutate(reqData);
+  };
+
+  return (
+    <div className={style.showUpdateProductContent}>
+      {data && (
+        <>
+          <ProductInfo {...data} />
+          <SetProductSoldBy
+            soldByStatus={data.sold_by}
+            handlerSoldBy={handlerSoldByProduct}
+          />
+        </>
+      )}
+    </div>
+  );
+};
+
+export { ShowUpdateProduct };
