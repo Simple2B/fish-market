8a3af4aeb7631b0459b0fd22e32d1aa0d31f3096
nazarKobryn <kobrinnazar05@gmail.com>
2023-01-10 18:35:16 +0200

    update style error mes and business and added new logic

--- frontend/src/pages/Market/components/BusinessBtn/BusinessBtn.tsx
+++ frontend/src/pages/Market/components/BusinessBtn/BusinessBtn.tsx
@@ -1,27 +1,23 @@
 import classNames from "classnames";
-import React from "react";
+import { PHONE_WIDTH } from "../../../../constants";
 import { useWindowDimensions } from "../../../../hooks/useWindowDimensions";
-import { PHONE_WIDTH } from "../../constants";
 import { IProduct } from "../../Market.type";
 import style from "./BusinessBtn.module.css";
 
 type BusinessBtnProps = {
   onClick: () => void;
   textBtn: string;
-  cartState: IProduct[];
+  isNotActivePhoneView: boolean;
 };
 
-const BusinessBtn = ({ onClick, textBtn, cartState }: BusinessBtnProps) => {
-  const { height, width } = useWindowDimensions();
-
+const BusinessBtn = ({
+  onClick,
+  textBtn,
+  isNotActivePhoneView,
+}: BusinessBtnProps) => {
   const styleBusinessBtn = classNames(style.businessBtn, {
-    [style.businessBtnInActive]: width <= PHONE_WIDTH && cartState.length <= 0,
+    [style.businessBtnInActive]: isNotActivePhoneView,
   });
-
-  console.log(
-    width <= PHONE_WIDTH && cartState.length,
-    " width <= PHONE_WIDTH && cartState.length"
-  );
 
   return (
     <div className={styleBusinessBtn} onClick={onClick}>
